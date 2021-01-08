091268adffcf1bfc69a1e90de6b3eb76fef62f2a
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-01-08 17:57:53 +0100

    add updated scripts

--- scripts_used_in_study/firmware_used_in_study.ino
+++ scripts_used_in_study/firmware_used_in_study.ino
@@ -1,21 +1,64 @@
-// set output pins
+/*
+Device firmware for an USB trigger box (Appelhoff & Stenner, 2021).
+MIT License
+Copyright (c) 2021 Tristan Stenner
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// Teensys can send emulated key presses to the host computer.
+// Enabling this option "presses" the Enter key every 90ms when pin 10 is
+// connected to ground. Enabled by default on Teensys when compiling with
+// keyboard support unless ENABLE_KEYBOARD=0 is defined
+#ifndef ENABLE_KEYBOARD
+#if defined(CORE_TEENSY_KEYBOARD) || defined(USB_SERIAL_HID)
+#define ENABLE_KEYBOARD 1
+#else
+#define ENABLE_KEYBOARD 0
+#endif
+#endif
+
+// send special pin patterns to troubleshoot connection issues after receiving
+// either 254 or 255 as value
+#define ENABLE_PIN_TESTS 0
+
+// function prototype; sets output pins in a loop
 void setOutputsLoop(uint8_t outChar);
 
-
+// device specific values for outputPins and setOutputs;
 #if defined(ARDUINO_AVR_MICRO)
 constexpr int outputPins[] = {2,3,4,5,6,7,8,9};
 auto setOutputs = setOutputsLoop;
-#elif defined(ARDUINO_AVR_LEONARDO)
+
+#elif defined(ARDUINO_AVR_LEONARDO) || defined(ARDUINO_AVR_UNO)
 constexpr int outputPins[] = {0,1,2,3,4,5,6,7};
 auto setOutputs = setOutputsLoop;
+
 #elif defined(CORE_TEENSY)
 constexpr int outputPins[] = {0,1,2,3,4,5,6,7};
+
 void setOutputs(uint8_t outChar) {
-	for(int i=0; i<8; ++i) digitalWriteFast(outputPins[i], outChar&(1<<i));
+  for(int i=0; i<8; ++i) digitalWriteFast(i, outChar&(1<<i));
 }
-elapsedMillis lastKbdEvent = 0;
+
 #else
-#error "Unknown board"
+#warning "Unknown board"
+// Fall back to default values
+constexpr int outputPins[] = {0,1,2,3,4,5,6,7};
+auto setOutputs = setOutputsLoop;
 #endif
 
 void setOutputsLoop(uint8_t outChar)
@@ -24,7 +67,7 @@ void setOutputsLoop(uint8_t outChar)
 }
 
 void clearOutputs() {
-  for(auto pin: outputPins) digitalWrite(pin, LOW);
+  setOutputs(0);
 }
 
 void setup() {
@@ -36,10 +79,13 @@ void setup() {
 }
 
 void loop() {
-#if defined(CORE_TEENSY_KEYBOARD) || defined(USB_SERIAL_HID)
-  if(!digitalRead(10) && lastKbdEvent >= 100) {
-    Keyboard.press(KEY_ENTER);
+#if ENABLE_KEYBOARD
+	// Helper variable to keep the time of the last synthetic keyboard event
+	static elapsedMillis lastKbdEvent = 0;
+
+  if(!digitalRead(10) && lastKbdEvent >= 90) {
     digitalWriteFast(outputPins[1], HIGH);
+    Keyboard.press(KEY_ENTER);
     delay(1);
     Keyboard.release(KEY_ENTER);
     digitalWriteFast(outputPins[1], LOW);
@@ -49,13 +95,12 @@ void loop() {
 #else
 #warning "Keyboard disabled"
 #endif
+
   if(!Serial.available()) return;
+  // read in a single value
   uint8_t inChar = Serial.read();
-  Serial.println((int) inChar);
-#if defined(CORE_TEENSY)
-  Serial.send_now();
-#endif
-  if(inChar==255)
+
+  if(ENABLE_PIN_TESTS && inChar==255)
     for(inChar=1; inChar<255; inChar++){
       setOutputs(inChar);
       delay(100);
@@ -63,7 +108,7 @@ void loop() {
       delay(100);
       Serial.println(inChar);
   }
-  else if(inChar==254)
+  else if(ENABLE_PIN_TESTS && inChar==254)
     for(int inChar=1;inChar<256;inChar = inChar << 1){
       setOutputs(inChar);
       delay(100);
@@ -72,10 +117,16 @@ void loop() {
     }
   else {
     setOutputs(inChar);
+    // also blink LED for visual feedback
     digitalWrite(13, HIGH);
     delay(5);
     clearOutputs();
     digitalWrite(13, LOW);
-    delay(2);
   }
+
+  // Optional: write back the received value
+  // Serial.println((int) inChar);
+#if defined(CORE_TEENSY)
+  // Serial.send_now();
+#endif
 }
--- scripts_used_in_study/script_used_in_study.py
+++ scripts_used_in_study/script_used_in_study.py
@@ -2,21 +2,37 @@
 
 import os
 import pylsl
-import serial
 import sys
 import time
 
-if os.name == 'nt':
-    from psychopy.parallel import ParallelPort as PP
+port = sys.argv[1]
+
+if port == 'parport':
+    if os.name == 'nt':
+        from psychopy.parallel import ParallelPort as PP
+    else:
+        from parallel import Parallel as PP
+    parport = PP()
+    def send_trigger():
+        parport.setData(1)
+        time.sleep(.005)
+        parport.setData(0)
+elif port == 'labjack':
+    import u3
+    lj = u3.U3()
+    def send_trigger():
+        lj.writeRegister(6700, 0xFF01)
+        # lj.setFIOState(0, 1)
+        time.sleep(.01)
+        lj.writeRegister(6700, 0xFF00)
+        # lj.setFIOState(0, 0)
 else:
-    from parallel import Parallel as PP
-
-com = sys.argv[1] if len(sys.argv) > 1 else '/dev/ttyACM0'
-
-# serial port
-ser = serial.Serial(com, 115200, xonxoff=False, rtscts=False, dsrdtr=False, write_timeout=0)
-# parallel port
-pp = PP()
+    # serial port
+    ser, _ = ptb.IOPort('OpenSerialPort', port, 'BaudRate=115200 FlowControl=None ReceiveTimeout=0.05')
+    def send_trigger():
+        ptb.IOPort('Write', ser, '\x01')
+        # flush incoming data
+        _, _, err = ptb.IOPort('Read', ser, 1, 3)
 
 # LSL outlet
 outlet = pylsl.StreamOutlet(
@@ -26,19 +42,41 @@ outlet = pylsl.StreamOutlet(
                      nominal_srate=pylsl.IRREGULAR_RATE,
                      channel_format=pylsl.cf_string))
 
+# get the psychHID index for the emulated keyboard
+try:
+    kbdidx = next((dev['index'] for dev in ptb.PsychHID('devices',4) if dev['product'].startswith('Teensyduino')))
+except StopIteration:
+    kbdidx = 0
+
+# initialize the Psychtoolbox keyboard queue
+keycode = 12 if os.name == 'nt' else 36
+keys = np.zeros(256)
+keys[keycode] = 1
+ptb.PsychHID('KbQueueCreate', kbdidx, keys)
+ptb.PsychHID('KbQueueStart', kbdidx)
+
+# call both functions once to make sure the C libraries are loaded
+pylsl.local_clock()
+ptb.GetSecs()
+# make sure LSL and PTB use the same underlying clock
+timediff = abs(pylsl.local_clock()-ptb.GetSecs()+ptb.GetSecs()-pylsl.local_clock())
+assert(timediff<1e-3)
+
 while True:
-    t = pylsl.local_clock()  # get trial start time
-    pp.setData(1)  # set parallel port output pin to high
-    ser.write(b'5')  # send the trigger to the serial device
-    time.sleep(.001)  # sleep 1ms
-    pp.setData(0)  # set the parallel port output pin to low
+    # wait for the Teensy to simulate a keypress
+    ptb.PsychHID('KbQueueFlush', kbdidx, 3);
+    event, n = ptb.PsychHID('KbQueueGetEvent', kbdidx, 1)
+    t0 = pylsl.local_clock()
+    # don't send a trigger when waiting for the keypress timed out or the event
+    # signified the key release (as opposed to initially pressing it down)
+    if len(event) == 0 or event['Pressed'] == 0:
+        continue
+
+    send_trigger()  # send a trigger via the configured interface
+
     # send an LSL event with the previously measured time to the LabStreamer
     outlet.do_push_sample(
         outlet.obj,
         outlet.sample_type(b'1'),
-        pylsl.pylsl.c_double(t),
+        pylsl.pylsl.c_double(t0),
         pylsl.pylsl.c_int(True))
-
-    # wait for the Teensy to simulate a keypress
-    sys.stdin.read(1)
-    ser.reset_input_buffer()
