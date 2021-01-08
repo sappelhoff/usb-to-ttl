d9c251543d11300b330f2a2088396c21a9c37bf4
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-01-08 18:10:40 +0100

    misc edits

--- scripts_used_in_study/firmware_used_in_study.ino
+++ scripts_used_in_study/firmware_used_in_study.ino
@@ -1,7 +1,10 @@
 /*
-Device firmware for an USB trigger box (Appelhoff & Stenner, 2021).
+Device firmware for a USB trigger box (Appelhoff & Stenner, 2021).
+
 MIT License
+
 Copyright (c) 2021 Tristan Stenner
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
--- scripts_used_in_study/script_used_in_study.py
+++ scripts_used_in_study/script_used_in_study.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python
+"""Measure TTL trigger sending latency of several devices.
+
+Required packages:
+
+- numpy
+- pylsl
+- psychtoolbox (https://pypi.org/project/psychtoolbox/)
+
+"""
 
 import os
-import pylsl
 import sys
 import time
 
+import numpy as np
+import pylsl
+import psychtoolbox as ptb
+
 port = sys.argv[1]
 
+# Define the "send_trigger" function depending on which device we are testing
 if port == 'parport':
     if os.name == 'nt':
         from psychopy.parallel import ParallelPort as PP
     else:
         from parallel import Parallel as PP
     parport = PP()
+
     def send_trigger():
         parport.setData(1)
         time.sleep(.005)
         parport.setData(0)
+
 elif port == 'labjack':
     import u3
     lj = u3.U3()
+
     def send_trigger():
+        # Using the U3, we can either use the writeRegister method, ...
+        # or the setFIOState method.
         lj.writeRegister(6700, 0xFF01)
         # lj.setFIOState(0, 1)
         time.sleep(.01)
         lj.writeRegister(6700, 0xFF00)
         # lj.setFIOState(0, 0)
+
 else:
     # serial port
     ser, _ = ptb.IOPort('OpenSerialPort', port, 'BaudRate=115200 FlowControl=None ReceiveTimeout=0.05')
+
     def send_trigger():
         ptb.IOPort('Write', ser, '\x01')
-        # flush incoming data
+        # "Sleep" is handled by firmware on device
+        # Flush incoming data
         _, _, err = ptb.IOPort('Read', ser, 1, 3)
 
-# LSL outlet
+# Create an LSL outlet
 outlet = pylsl.StreamOutlet(
     pylsl.StreamInfo(name='latencytest',
                      type='marker',
@@ -42,39 +63,40 @@ outlet = pylsl.StreamOutlet(
                      nominal_srate=pylsl.IRREGULAR_RATE,
                      channel_format=pylsl.cf_string))
 
-# get the psychHID index for the emulated keyboard
+# Get the psychHID index for the emulated keyboard
 try:
-    kbdidx = next((dev['index'] for dev in ptb.PsychHID('devices',4) if dev['product'].startswith('Teensyduino')))
+    kbdidx = next((dev['index'] for dev in ptb.PsychHID('devices', 4) if dev['product'].startswith('Teensyduino')))
 except StopIteration:
     kbdidx = 0
 
-# initialize the Psychtoolbox keyboard queue
+# Initialize the Psychtoolbox keyboard queue
 keycode = 12 if os.name == 'nt' else 36
 keys = np.zeros(256)
 keys[keycode] = 1
 ptb.PsychHID('KbQueueCreate', kbdidx, keys)
 ptb.PsychHID('KbQueueStart', kbdidx)
 
-# call both functions once to make sure the C libraries are loaded
+# Call both functions once to make sure the C libraries are loaded
 pylsl.local_clock()
 ptb.GetSecs()
-# make sure LSL and PTB use the same underlying clock
+
+# Make sure LSL and PTB use the same underlying clock
 timediff = abs(pylsl.local_clock()-ptb.GetSecs()+ptb.GetSecs()-pylsl.local_clock())
-assert(timediff<1e-3)
+assert(timediff < 1e-3)
 
 while True:
-    # wait for the Teensy to simulate a keypress
-    ptb.PsychHID('KbQueueFlush', kbdidx, 3);
+    # Wait for the Teensy to simulate a keypress
+    ptb.PsychHID('KbQueueFlush', kbdidx, 3)
     event, n = ptb.PsychHID('KbQueueGetEvent', kbdidx, 1)
     t0 = pylsl.local_clock()
-    # don't send a trigger when waiting for the keypress timed out or the event
+    # Don't send a trigger when waiting for the keypress timed out or the event
     # signified the key release (as opposed to initially pressing it down)
     if len(event) == 0 or event['Pressed'] == 0:
         continue
 
-    send_trigger()  # send a trigger via the configured interface
+    send_trigger()  # Send a trigger via the configured interface
 
-    # send an LSL event with the previously measured time to the LabStreamer
+    # Send an LSL event with the previously measured time to the LabStreamer
     outlet.do_push_sample(
         outlet.obj,
         outlet.sample_type(b'1'),
